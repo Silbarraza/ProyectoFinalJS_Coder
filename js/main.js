17f602ca5b7e5a0e7de09548b622863ac9a2b8d6
Silvana Barraza <barrazasilvana@gmail.com>
2022-11-29 18:28:24 -0300

    modificaciones en el codigo js

--- js/main.js
+++ js/main.js
@@ -1,87 +1,3 @@
-//Se tomaran los personajes de la api como clientes de la base de datos
-/* fetch('https://swapi.dev/api/people')
-.then((response)=>{
-    return response.json()
-}).then ((personasStarwars)=>{
-      
-    const personas = personasStarwars.results
-
-    for (const persona of personas) {
-        
-        const nombreClienteRegistrado = persona.name
-        const anioDeNacimientoCliente = persona.birth_year
-
-        console.log(nombreClienteRegistrado,anioDeNacimientoCliente)
-    }
-}) */
-
-/* FUNCIONES */
-/* async function obtenerPersonasStarwars (){
-    const response =await fetch ('https://swapi.dev/api/people')
-    const personasStarwars  = await response.json()
-
-    console.log(personasStarwars)
-
-    const personasStarwarsList = personasStarwars.results
-    
-    for (const persona of personasStarwarsList){
-        const nombreClienteRegistrado = persona.name
-        const anioDeNacimientoCliente = persona.birth_year
-
-        console.log(nombreClienteRegistrado,anioDeNacimientoCliente)
-    }
-   
-}
-
-obtenerPersonasStarwars() */
-
-
-/* Swal.fire({
-    title: 'Login',
-    html: `<input type="text" id="login" class="swal2-input" placeholder="Nombre y Apellido">
-    <input type="password" id="password" class="swal2-input" placeholder="Año de Nacimiento">`,
-    confirmButtonText: 'Ingresar',
-    focusConfirm: false,
-    preConfirm: () => {
-      const login = Swal.getPopup().querySelector('#login').value
-      const password = Swal.getPopup().querySelector('#password').value
-      if (!login || !password) {
-        Swal.showValidationMessage(`Por favor ingresa tus datos`)
-      }
-      return { login: login, password: password }
-    }
-  }).then((result) => {
-    if (result.isConfirmed) {
-        Swal.fire(`
-        Bienvenid@ 
-        ${result.value.login}
-        `.trim())
-    }
-}) */
-
-/* VARIABLES */
-//localStorage.clear();
-
-/* const formularioLoginCliente = document.getElementById("formularioLogin")
-
-const inputNombreYapellido = document.getElementById("inputNombreYapellido")
-let inputClave = document.getElementById("inputClave")
-
-
-
-formularioLoginCliente.addEventListener("submit", (event) => {
-
-    event.preventDefault()
-
-    //Obtenemos los datos
-    const nombreYapellido = inputNombreYapellido.value
-    let clave = inputClave.value
-
-    console.log(nombreYapellido,clave)
-
-    
-}) */
-
 //localStorage.clear();
 
 /* FUNCIONES */
@@ -131,7 +47,8 @@ function crearInputs (){
     inputFecha.min = `${fechaHoy.getFullYear()}-${fechaHoy.getMonth() + 1}-${fechaHoy.getDate()}`;
     inputFecha.style.padding="5px";
     inputFecha.style.margin="10px";
-
+    inputFecha.style.display="block";
+    
     //Creo input Nombre y apellido
     inputNombreYapellido= document.createElement("input");
     inputNombreYapellido.placeholder="Nombre y Apellido";
@@ -149,27 +66,19 @@ function crearInputs (){
 
     //Agregamos input al formulario
 
-    datosCliente.append(inputFecha,inputNombreYapellido,inputIdCliente);
+    datosCliente.append(inputNombreYapellido,inputIdCliente,inputFecha);
 
     formularioReservas.append(datosCliente)
     agregarElementosAlFormulario();
 }
 
-
 function fechaSeaMayorAhoy(fecha){
 
     const dateHoy = new Date();
     const dateReserva = new Date(fecha);
+    const operadorternario = (dateReserva>dateHoy) ? true: false;
     
-    //const anioFechaReservada = dateReserva.getFullYear();
-    //const mesFechaReservada = dateReserva.getMonth();
-    //const diaRechaReservada = dateReserva.getDate();
-
-    //Validar si la fecha elegida es menor a hoy
-    if(dateReserva < dateHoy){
-        return false;
-    }
-    return true;
+    return operadorternario;
 }
 
 function obtenerReservas () {
@@ -196,48 +105,49 @@ function agregarElementosAlFormulario (){
     formularioReservas.append(btnReserva);
 }
 
-function mensajeReserva () {
-    let btnReserva = document.querySelector('#btnReserva');
-    
-    btnReserva.addEventListener('click', () => {
+/* VARIABLES GLOBALES */
+//localStorage.clear();
 
-        Swal.fire({
-            title: 'Genial!',
-            text: 'Ya reservaste tu lugar en la clase!',
-            icon: 'success',
-            confirmButtonText: 'Ok'
-    })
-    })
-}
-
-//let personasStarwarsList ;
-//let existeCliente = "";
+const formularioReservas = document.getElementById("reserva");
+const datosCliente = document.getElementById("datosCliente");
+const datosReserva = document.getElementById("datosReserva");
+let btnReserva = document.getElementById("btnReserva");
 
 
-/* 
-async function obtenerPersonasStarwars (){
-    const response =await fetch ('https://swapi.dev/api/people')
-    const personasStarwars  = await response.json()
-    
-    personasStarwarsList = personasStarwars.results
-    console.log(personasStarwars.results)
-    
+let inputFecha;
+let inputNombreYapellido;
+let inputIdCliente;
+crearInputs();
 
-   for (const persona of personasStarwarsList){
-        const nombreClienteRegistrado = persona.name
-        const anioDeNacimientoCliente = persona.birth_year
-        console.log(nombreClienteRegistrado,anioDeNacimientoCliente) 
-    }
-    //console.log(listaClientesRegistrados)
-    //return nombreClienteRegistrado
-}
-obtenerPersonasStarwars() */
+let reservas = obtenerReservas();
 
+const tbodyClasesReservadas = document.getElementById("tbodyClasesReservadas"); 
 
+let selectClase = document.getElementById("selectClase");
 
+/* CODIGO */
+renderizarTabla();
 
-//nota: hay que comparar los inputs de nombre y id con los datos de la API, ver como hacerlo
-function existeCliente (nombreYapellido, idCliente) {
+/*EVENTOS */
+
+formularioReservas.addEventListener("submit", (event)=>{
+
+    //Detenemos el evento
+    event.preventDefault();
+
+    //Obtenemos los datos
+    const fecha = inputFecha.value;
+    const nombreYapellido = inputNombreYapellido.value;
+    const idCliente = parseInt(inputIdCliente.value);
+
+    //Obtenemos la clase seleccionada   
+    selectClase.addEventListener("change", (event)=>{
+        valor = event.target.value;
+        return valor;
+    }); 
+
+    let nombreClientesEnBaseDeDatos;
+    let idClientesEnBaseDeDatos;
 
     fetch('/mi_repositorio/json/clientes.json')
     .then( (response) => {
@@ -246,137 +156,75 @@ function existeCliente (nombreYapellido, idCliente) {
     
     }).then( (clientes) => {
 
-        let clienteEncontrado = true;
+        nombreClientesEnBaseDeDatos = clientes.some( function (cliente) {
+            return cliente.nombreYapellido.toLowerCase() === nombreYapellido.toLowerCase();
+        }); 
+        console.log(nombreClientesEnBaseDeDatos)
 
-        for (let i=0; i < clientes.length; i++){
-            
-            if((clientes[i].nombreYapellido !== nombreYapellido)&&(clientes[i].idCliente !== idCliente)){
-                clienteEncontrado=false;
+        idClientesEnBaseDeDatos = clientes.some( function (cliente) {
+            return cliente.idCliente === idCliente;
+        }); 
+        console.log(nombreClientesEnBaseDeDatos);
 
-                Swal.fire({
-                    icon: 'warning',
-                    title: 'Oops...',
-                    text: 'Cliente no existe!'
-                })
+        //Comprobamos que el cliente este en la base de datos
+        if((!nombreClientesEnBaseDeDatos)||(!idClientesEnBaseDeDatos)){
 
-                console.log("Cliente no existe")
-                //return true;
-                break;
-                
-                
-
-            }
-        }
-        return clienteEncontrado;
-    });
-
-    
-
-}
-
-/* VARIABLES GLOBALES */
-//localStorage.clear();
-
-const formularioReservas = document.getElementById("reserva");
-const datosCliente = document.getElementById("datosCliente");
-const datosReserva = document.getElementById("datosReserva");
-
-
-
-let inputFecha;
-let inputNombreYapellido;
-let inputIdCliente;
-crearInputs();
-
-//const inputNombreYapellido = document.getElementById("nombreYapellido"); 
-//const inputIdCliente = document.getElementById("idCliente"); 
-
-let reservas = obtenerReservas();
-
-const tbodyClasesReservadas = document.getElementById("tbodyClasesReservadas"); 
-
-let selectClase = document.getElementById("selectClase");
-
-let valor;
-
-//const listaClientes = [];
-
-//console.log(listaClientes)
-//console.log(selectClase);
-
-
-/* CODIGO */
-renderizarTabla();
-
-/*EVENTOS */
-
-formularioReservas.addEventListener("submit", (event)=>{
-    
-    //Detenemos el evento
-    event.preventDefault();
-
-    //Obtenemos los datos
-    const fecha = inputFecha.value;
-    const nombreYapellido = inputNombreYapellido.value;
-    const idCliente = parseInt(inputIdCliente.value);
-    
-    //Comprobar si existe el cliente
-    existeCliente(nombreYapellido, idCliente);
-
-    //Obtenemos la clase seleccionada   
-    selectClase.addEventListener("change", (event)=>{
-        valor = event.target.value;
-        return valor;
-    }); 
-
-    //Chequeamos que la fecha no este reservada y que la fehca sea distinta de hoy
-    if(fechaDisponible(fecha)) {
-
-        // Chequeamos que la fecha elegida sea mayor al día de hoy
-        if (fechaSeaMayorAhoy(fecha)){
-            //Cargamos la reserva al array
-            reservas.push({
-            fecha: fecha,
-            nombreYapellido: nombreYapellido,
-            idCliente: idCliente,
-            clase: selectClase.value,
+            Swal.fire({
+                icon: 'warning',
+                title: 'Oops...',
+                text: 'Cliente no existe!'
             });
-
-            //Cargar el array al localstorage
-            localStorage.setItem("reservas", JSON.stringify(reservas));
-
-            mensajeReserva();
-
-            //alert ("Clase reservada con éxito");
 
             //Limpiar inputs
             inputFecha.value="";
             inputNombreYapellido.value="";
             inputIdCliente.value="";
+
+        } else if (fechaDisponible(fecha)){
+
+            if(fechaSeaMayorAhoy(fecha)){
+                
+                //Cargamos la reserva al array
+                reservas.push({
+                fecha: fecha,
+                nombreYapellido: nombreYapellido,
+                idCliente: idCliente,
+                clase: selectClase.value,
+                });
     
-            //Renderizar tabla
-            renderizarTabla();
+                //Cargar el array al localstorage
+                localStorage.setItem("reservas", JSON.stringify(reservas));
+                console.log(reservas)
+                //mensaje reserva exitosa
+                Swal.fire({
+                    title: 'Genial!',
+                    text: 'Ya reservaste tu lugar en la clase!',
+                    icon: 'success',
+                    confirmButtonText: 'Ok'
+                })
+
+                //Limpiar inputs
+                inputFecha.value="";
+                inputNombreYapellido.value="";
+                inputIdCliente.value="";
+    
+                //Renderizar tabla
+                renderizarTabla();
+
+            }else{
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'La fecha es incorrecta!'
+                })
+            }
 
         }else {
-
             Swal.fire({
-                icon: 'error',
+                icon: 'warning',
                 title: 'Oops...',
-                text: 'La fecha es incorrecta!'
+                text: 'Esta fecha ya esta reservada!'
             })
-
         }
-    
-
-    }else {
-
-        Swal.fire({
-            icon: 'warning',
-            title: 'Oops...',
-            text: 'Esta fecha ya esta reservada!'
-        })
-    }   
-
-
+    });
 });
-
